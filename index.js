c38c0ff0994f8c828ef29ee8fec28c48e46953e7
Lauriacunia <lauriacunia@gmail.com>
2020-11-27 11:31:14 -0300

    Corrijo funcion buscar matches en linea y agrego buscar en columnas

--- index.js
+++ index.js
@@ -106,28 +106,50 @@ function dragStart () {
 // Buscar Matches
 // buscar en filas-- horizontales
 
-function checkRowForThree() {
-  console.log("estas buscando maches en linea")
 
-  for(i = 0; i < 61; i++) {
-    console.log("estas dentro del for")
+function checkRowForThree() {
+  for (i = 0; i < 61; i ++) {
     let rowOfThree = [i, i+1, i+2]
     let decidedColor = squares[i].style.backgroundColor
-    console.log(decidedColor)
-    const isBlank = squares[i].style.background === ''
-    
+    const isBlank = squares[i].style.backgroundColor === ''
+
+    //const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+    //if (notValid.includes(i)) continue
+
     if(rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
       score += 3
+      //scoreDisplay.innerHTML = score
       rowOfThree.forEach(index => {
       squares[index].style.backgroundColor = ''
       })
-
     }
   }
 }
-
 checkRowForThree()
 
+function checkColumnForThree() {
+  for (i = 0; i < 47; i ++) {
+    let columnOfThree = [i, i+width, i+width*2]
+    let decidedColor = squares[i].style.backgroundColor
+    const isBlank = squares[i].style.backgroundColor === ''
+
+    if(columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+      score += 3
+      //scoreDisplay.innerHTML = score
+      columnOfThree.forEach(index => {
+      squares[index].style.backgroundColor = ''
+      })
+    }
+  }
+}
+checkColumnForThree()
+
+
+window.setInterval(function(){
+  checkRowForThree()
+  checkColumnForThree()
+  
+}, 100);
 
 
 
